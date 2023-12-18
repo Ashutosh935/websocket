2235e7c01812b7ceaaa65dc7b264a125d3da3782
Ashutosh935 <71822244+Ashutosh935@users.noreply.github.com>
2023-12-18 20:30:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ app = FastAPI()
 async def get(inp):
     return {'response':inp}
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        while True:
-            data = await websocket.receive_text()
-            #query=eval(data)['content']
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             #query=eval(data)['content']
             
             
-            await websocket.send_text(data)
+#             await websocket.send_text(data)
            
                
-    except WebSocketDisconnect:
-        pass
+#     except WebSocketDisconnect:
+#         pass
 
 if __name__ == '__main__':
     uvicorn.run(app, port = 8080, host = '0.0.0.0') 
