bef80140a4ad4b95067c340883d743c8a547568b
Ashutosh935 <71822244+Ashutosh935@users.noreply.github.com>
2023-12-19 22:47:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,5 +25,29 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         pass
 
+
+async def astream_log():
+    for i in range(5):
+        yield f"Result {i}"
+
+
+@app.websocket("/ws2")
+async def websocket_endpoint2(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print('################',data)
+            query=eval(data)['content']
+            async for result in astream_log():
+               
+                await websocket.send_text(result)
+                await asyncio.sleep(1)
+
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
+        await websocket.close()
+
+
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
